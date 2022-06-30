8354a5c4c703de606a1cc3e94b43666319a0fa83
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-06-30 22:54:39 +0300

    Добавляет Task -08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const formEl = document.querySelector('.login-form');
+
+
+// отправка формі
+function onFormSubmit(event) {
+    // очистка
+    event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+    const email = formElem.email.value;
+    const password = formElem.password.value;
+
+    // валидация
+    if (email === '' || password === '') {
+        return alert('Все поля должны быть заполнены');
+    }
+    // группировка в объект
+    const formData = {
+        email, password
+    };
+    console.log(formData);
+    // очистка формы
+    event.currentTarget.reset();
+}
+
+
+
+formEl.addEventListener('submit', onFormSubmit);
+
 
