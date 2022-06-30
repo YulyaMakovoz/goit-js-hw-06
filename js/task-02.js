cb1bbf6967eb2c7b2b8b48fa49757dd7b6923c3c
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-06-30 16:19:43 +0300

    добавляет task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// const ingredient = [...ingredients];
+// const itemEl = document.createElement('li');
+// itemEl.textContent = ingredient[0];
+// itemEl.classList.add('item');
+// console.log(itemEl);
+
+const ingredientsEl = document.querySelector('#ingredients');
+// ingredientsEl.append(itemEl);
+
+const makeItem = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+    
+    return itemEl;
+  });
+};
+
+// console.log(itemEl);
+
+const elements = makeItem(ingredients);
+ingredientsEl.append(...elements);
+
+
+
