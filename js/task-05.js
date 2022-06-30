dd9099cd59b61c6e3496a78493f91c59aae9fb8f
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-06-30 21:59:14 +0300

    Меняет task-05

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,11 @@ const textOutput = document.querySelector('#name-output');
 function onInput(event) {
     textOutput.textContent = event.currentTarget.value;
 }
-
+function onAnonim(event) {
+    if (event.currentTarget.value.length === +0) {
+        return textOutput.textContent = "Anonymous"
+    }
+}
 
 textInput.addEventListener('input', onInput);
+textInput.addEventListener('blur', onAnonim);
