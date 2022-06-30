c7f7be3537b1f55f4403f4aba9aa1c2db5355d78
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-06-30 23:43:03 +0300

    Добавляет Task-09, меняет Task -06

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,14 @@
 const inputEl = document.querySelector('#validation-input');
 console.dir(inputEl);
 function onVerification(event) {
-    if (event.currentTarget.value.length === Number(6)) {
+    if (event.currentTarget.value.trim().length === Number(inputEl.dataset.length)) {
         return inputEl.classList.add('valid');
     }
     return inputEl.classList.add('invalid');
+    
 }
+
 inputEl.addEventListener('blur', onVerification);
 
+// console.log(event.currentTarget.value.length);
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
+
+const bodyEl = document.body;
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+function onChangeColor() {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+buttonEl.addEventListener('click', onChangeColor)
+
+
+
