721ad3b7a254b4f7fed8e08d8d3c1f0e24dc5806
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-07-02 15:27:48 +0300

    Исправляет Task-6, Task -6

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,14 @@
 const textInput = document.querySelector('#name-input');
 const textOutput = document.querySelector('#name-output');
 
+
 function onInput(event) {
-    textOutput.textContent = event.currentTarget.value;
-}
-function onAnonim(event) {
-    if (event.currentTarget.value.length === +0) {
-        return textOutput.textContent = "Anonymous"
+    if (event.currentTarget.value !== "") {
+     return textOutput.textContent = event.currentTarget.value;   
     }
+    return textOutput.textContent = "Anonymous"
 }
 
+
 textInput.addEventListener('input', onInput);
-textInput.addEventListener('blur', onAnonim);
+
--- js/task-06.js
+++ js/task-06.js
@@ -2,13 +2,15 @@ const inputEl = document.querySelector('#validation-input');
 console.dir(inputEl);
 function onVerification(event) {
     if (event.currentTarget.value.trim().length === Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('invalid');
         return inputEl.classList.add('valid');
     }
+    inputEl.classList.remove('valid');
     return inputEl.classList.add('invalid');
     
 }
 
 inputEl.addEventListener('blur', onVerification);
 
-// console.log(event.currentTarget.value.length);
+
 
